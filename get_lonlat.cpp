c452921b7e6e4871868cab13b9cf69783322aa54
feludwig <fludwig@student.ethz.ch>
2023-09-27 21:37:49 +0200

    cleanup and add comments

--- get_lonlat.cpp
+++ get_lonlat.cpp
@@ -6,38 +6,32 @@
 #include <iomanip>
 #include "node-persistent-cache.hpp"
 
-int main(int argc, char *argv[])
-{
-
+int main(int argc, char *argv[]) {
     if (argc<2) {
-	std::cerr << "Usage: "<<argv[0]<<" [flatondes_file]" << std::endl;
-	std::cerr << "\tThen write one {osm_id} per input line," << std::endl;
-	std::cerr << "\tWill print one-line results: {lon};{lat};{osm_id}" << std::endl;
+        std::cerr << "Usage: "<<argv[0]<<" [flatondes_file]" << std::endl;
+        std::cerr << "\tThen write one {osm_id} per input line," << std::endl;
+        std::cerr << "\tWill print one-line results: {lon};{lat};{osm_id}" << std::endl;
         return 0;
     }
+    // initialize, for now needs argv[1] the filename to be readwrite access, O_RDWR.
+    // changing to O_RDONLY does not compile and is a deeper issue than a oneline change
     node_persistent_cache n(argv[1], false);
     osmid_t osm_id;
-    //char* osm_id_str_prev=argv[2];
-    //char* osm_id_str=argv[2];
     std::cin>>osm_id;
     while (!std::cin.eof()) {
-      //osm_id_str=strchr(osm_id_str_prev,',');
-      //if (osm_id_str!=NULL) {
-        //*osm_id_str='\0';
-      //}
-      //sscanf(osm_id_str_prev,"%ld",&osm_id);
-      //osm_id_str_prev=osm_id_str+1;
-      try {
-        osmium::Location result=n.get(osm_id);
-        result.as_string(std::ostream_iterator<char>(std::cout), ';');
-        std::cout << ';' << osm_id << std::endl;
-      } catch (osmium::invalid_location &err) {
-        //std::cerr<<"skipping id "<<osm_id<<'\n';
-      }
-      std::cin>>osm_id;
-      //if (osm_id_str==NULL) {
-        //break;
-      //}
+        try {
+            osmium::Location result=n.get(osm_id);
+            // write Location.lat and Location.lon to stdout, ';'-separated
+            result.as_string(std::ostream_iterator<char>(std::cout), ';');
+            // add osm_id
+            std::cout << ';' << osm_id << std::endl;
+        } catch (osmium::invalid_location &err) {
+          // don't clutter stderr
+          //std::cerr<<"skipping id "<<osm_id<<'\n';
+        }
+        // continually read new ids until EOF: don't wait until stdin is
+        // closed before processing all the ids
+        std::cin>>osm_id;
     }
     return 0;
 }
